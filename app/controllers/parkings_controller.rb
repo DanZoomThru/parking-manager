c71a783c836cc5f33d70e84f7e4e8e3c14f26afa
Jean Anquetil <anquetiljean@gmail.com>
2016-10-17 13:06:14 +0200

    parkings#new & parkings#create done

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -2,4 +2,24 @@ class ParkingsController < ApplicationController
   def index
     @parkings = Parking.all
   end
+
+  def new
+    @parking = Parking.new
+  end
+
+  def create
+    @parking = Parking.new(parking_params)
+    if @parking.save
+      redirect_to "/"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def parking_params
+    params.require(:parking).permit(:name, :address, :available, :has_camera, :has_watchman, :city, :zip_code, :main_picture, :price_month)
+  end
+
 end
