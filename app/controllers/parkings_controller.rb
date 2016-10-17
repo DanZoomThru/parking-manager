0d31e0f8cb968001d2cdece5244781ac9dd9df0e
Jean Anquetil <anquetiljean@gmail.com>
2016-10-17 14:08:16 +0200

    parkings#destroy done

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,4 +1,6 @@
 class ParkingsController < ApplicationController
+  before_action :set_parking, only: [:edit, :update, :destroy]
+
   def index
     @parkings = Parking.all
   end
@@ -9,6 +11,10 @@ class ParkingsController < ApplicationController
 
   def create
     @parking = Parking.new(parking_params)
+    @parking.district = @parking.zip_code[0..1] == "75" ? @parking.zip_code[3..4] : "0"
+    gps = Geokit::Geocoders::GoogleGeocoder.geocode(@parking.address + ", " + @parking.city)
+    @parking.lat = gps.ll.split(',').first.to_f
+    @parking.lng = gps.ll.split(',').last.to_f
     if @parking.save
       redirect_to "/"
     else
@@ -16,10 +22,19 @@ class ParkingsController < ApplicationController
     end
   end
 
+  def destroy
+    @parking.destroy
+    redirect_to "/"
+  end
+
   private
 
   def parking_params
     params.require(:parking).permit(:name, :address, :available, :has_camera, :has_watchman, :city, :zip_code, :main_picture, :price_month)
+  end
+
+  def set_parking
+    @parking = Parking.find(params[:id])
   end
 
 end
--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,2 +1,5 @@
 class Parking < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
 end
